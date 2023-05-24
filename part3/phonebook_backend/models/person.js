66f1eab460d813c1f95d1feca82647b4cf3ac9b3
Phi Dang <phictin@gmail.com>
2023-05-24 18:10:38 +0300

    add schema rules and validators

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -7,17 +7,43 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
+const phoneNumberValidators = [
+  {
+    // have length of 8 or more
+    validator: (number) => {
+      return number.length >= 8
+    },
+    message: 'phone number must have 8 digits or more',
+  },
+  {
+    // be formed of two parts that are separated by -, the first part has two or three numbers
+    // and the second part also consists of numbers
+    validator: (number) => {
+      return /^\d{2,3}-\d{5,}$/.test(number)
+    },
+    message: 'incorrect phone number format'
+  }
+]
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: [true, 'person name required'],
+  },
+  number: {
+    type: String,
+    validate: phoneNumberValidators,
+    required: [true, 'person phone number required'],
+  },
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
