75ee47033e037c18fef90208bf8cbabf544157b7
Phi Dang <phictin@gmail.com>
2023-06-16 17:50:20 +0300

    refactor & add test for post and put

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -10,8 +10,7 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-describe('Some tests', () => {
-
+describe('when there is initially some blogs saved', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -19,11 +18,20 @@ describe('Some tests', () => {
       .expect('Content-Type', /application\/json/)
   })
 
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
   test('unique identifier is id instead of _id', async () => {
     const response = await api.get("/api/blogs")
     const ids = response.body.map((blog) => blog.id)
     ids.forEach(id => expect(id).toBeDefined())
   })
+})
+
+describe("addition of a new blog", () => {
 
   test('a valid post can be added', async () => {
     const newPost = {
@@ -78,8 +86,47 @@ describe('Some tests', () => {
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
-
 })
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
+describe('update of a blog', () => {
+  test('succeeds with status code 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({ likes: 20501 })
+      .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length
+    )
+
+    const updatedBlog = blogsAtEnd[0]
+    expect(updatedBlog.likes).toBe(20501)
+  })
+})
+
 
 
 afterAll(async () => {
