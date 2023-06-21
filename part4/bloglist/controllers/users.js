0b3ecf8fcaf287cd1ed1398077fc2d4fc1672b3c
Phi Dang <phictin@gmail.com>
2023-06-21 21:51:40 +0300

    tests for user

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,12 +3,30 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({})
+    .populate("blogs", {
+      title: 1,
+      author: 1,
+      url: 1,
+    })
   response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (!password) {
+    return response.status(400).json({
+      error: "password must be given",
+    })
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error: "both username and password must be at least 3 characters long",
+    })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
