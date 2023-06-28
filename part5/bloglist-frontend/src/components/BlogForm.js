8b9eb4c8dfaefb7db8e69d496fa9ea8d20246bd9
Phi Dang <phictin@gmail.com>
2023-06-28 21:57:25 +0300

    refactor by separate components

---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: ''
+  })
+
+  const handleBlogChange = (event) => {
+    const { name, value } = event.target
+    setNewBlog({ ...newBlog, [name]: value })
+  }
+
+  const handleCreateNewBlog = async (event) => {
+    event.preventDefault()
+    createBlog(newBlog)
+    setNewBlog({ title: '', author: '', url: '' })
+  }
+
+  return (
+    <div>
+      <h2>create new blog</h2>
+      <form onSubmit={handleCreateNewBlog}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={newBlog.title}
+            name="title"
+            onChange={handleBlogChange}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={newBlog.author}
+            name="author"
+            onChange={handleBlogChange}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={newBlog.url}
+            name="url"
+            onChange={handleBlogChange}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
