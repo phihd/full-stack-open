46a8cff40a7c6ea502614d45c9b451ad0c7fda8a
Phi Dang <phictin@gmail.com>
2023-06-28 21:56:50 +0300

    refactor by moving blog into its own component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,67 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react'
+import { useState } from 'react'
+
+const Blog = ({ blog, updateLikes, deleteBlog, current_username }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const handleLike = () => {
+    const blogToUpdate = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+      user: blog.user.id,
+    }
+    updateLikes(blog.id, blogToUpdate)
+  }
+
+  const handleDelete = () => {
+    if (window.confirm(
+      `Remove blog ${blog.title} by ${blog.author}?`
+    )) {
+      deleteBlog(blog.id)
+    }
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} {' '}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div >
+          <div>{blog.url}</div>
+          <div>
+            Likes: {blog.likes}{' '}
+            <button onClick={handleLike}>
+              like
+            </button>
+          </div>
+          <div>{blog.user.name}</div>
+          {blog.user.username === current_username && (
+            <button onClick={handleDelete}>
+              delete
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
