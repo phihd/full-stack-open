11b35ba72cd977d59baeb22c596adef18e5e7360
Phi Dang <phictin@gmail.com>
2023-07-03 17:22:35 +0300

    add anecdote 6.4

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from "./reducers/anecdoteReducer"
+import { voteAnecdote, addAnecdote } from "./reducers/anecdoteReducer"
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
+  const handleVote = (id) => {
     dispatch(voteAnecdote(id))
+  }
+
+  const handleAddAnecdote = (event) => {
+    event.preventDefault()
+    dispatch(addAnecdote(event.target.anecdote.value))
+    event.target.anecdote.value = ''
   }
 
   return (
@@ -20,13 +26,15 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
           </div>
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={handleAddAnecdote}>
+        <div>
+          <input name='anecdote' />
+        </div>
         <button>create</button>
       </form>
     </div>
