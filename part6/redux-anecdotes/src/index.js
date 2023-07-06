8e78202c3e173cc97b3e74c7b88885c2b4d811f2
Phi Dang <phictin@gmail.com>
2023-07-06 16:51:50 +0300

    minor fixes

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 const store = configureStore({
   reducer: {
@@ -18,7 +16,7 @@ const store = configureStore({
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
+  <Provider store={store}>
     <App />
-  </QueryClientProvider>
+  </Provider>
 )
